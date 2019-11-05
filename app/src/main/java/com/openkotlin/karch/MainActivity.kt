2b844ec7cecb2d6248a2eb61ae650c0fc3714f7d
Tanck <tanck.tang@pb.com>
2019-11-05 12:00:07 +0800

    Updating the UI with viewModel

--- app/src/main/java/com/openkotlin/karch/MainActivity.kt
+++ app/src/main/java/com/openkotlin/karch/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.openkotlin.karch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.observe
-import com.openkotlin.karch.viewmodels.HomeViewModel as HomeViewModel1
+import com.openkotlin.karch.databinding.ActivityMainBinding
+import com.openkotlin.karch.viewmodels.HomeViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,14 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        val apply =
-//            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val activityMainBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+                .apply {
+                    viewModel = ViewModelProviders.of(this@MainActivity).get(HomeViewModel::class.java)
+                }
 //        apply.viewModel.name.
-//        setContentView(R.layout.activity_main)
-        val homeViewModel = ViewModelProviders.of(this).get(HomeViewModel1::class.java)
+
+
+
+
+        activityMainBinding.viewModel!!.mutableLiveData.observe(this) {
+            Log.d("Tanck", it)
+        }
         // lifecycle.addObserver(this)
         // TODO : The callback can be ignored in Kotlin
-        homeViewModel.mutableLiveData.observe(this, onChanged = {
-            it.length
-        })
+//        viewModel.mutableLiveData.observe(this, onChanged = {
+//
+//        })
     }
 }
