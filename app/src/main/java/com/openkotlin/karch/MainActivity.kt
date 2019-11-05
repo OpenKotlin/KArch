5dc737a58e5ce98b1b89f33429db20ef765c9762
Tanck <tanck.tang@pb.com>
2019-11-05 12:10:39 +0800

    Cleaned up code

--- app/src/main/java/com/openkotlin/karch/MainActivity.kt
+++ app/src/main/java/com/openkotlin/karch/MainActivity.kt
@@ -14,24 +14,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val apply =
         val activityMainBinding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
                 .apply {
                     viewModel = ViewModelProviders.of(this@MainActivity).get(HomeViewModel::class.java)
                 }
-//        apply.viewModel.name.
 
 
 
-
-        activityMainBinding.viewModel!!.mutableLiveData.observe(this) {
-            Log.d("Tanck", it)
-        }
-        // lifecycle.addObserver(this)
         // TODO : The callback can be ignored in Kotlin
 //        viewModel.mutableLiveData.observe(this, onChanged = {
 //
 //        })
+        activityMainBinding.viewModel!!.mutableLiveData.observe(this) {
+            Log.d("Tanck", it)
+        }
+
+
     }
 }
