59a94d584e0c033c414345f5bc3d14fb48dcff00
Tanck <tanck.tang@pb.com>
2019-11-12 15:55:45 +0800

    Adding some comments for java/kotlin

--- app/src/test/java/com/openkotlin/karch/ExampleUnitTest.kt
+++ app/src/test/java/com/openkotlin/karch/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    // StringBuild/StringBuffer 默认大小是16kb. 当发生扩容的时候,将会创建2倍.
+
+    // ArraryList/Vector 默认大小是10个List. 当发生扩容的时候,将会创建1.5倍的大小,并且会调用ArraryList.copy()拷贝整个list
+
+    // HashMap/HashTable 默认是16大小,负载因子是:0.75. 扩容出发为:当前定义的大小*负载因子 > 实际大小
 }
